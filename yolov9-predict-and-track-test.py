90353f2657a8cb017a627ed2401808c0f2c5c872
li zan <li-zan@foxmail.com>
2024-08-05 22:14:59 +0800

    :white_check_mark:add yolov9-predict-and-track-test.py

--- yolov9-predict-and-track-test.py
+++ yolov9-predict-and-track-test.py
@@ -3,25 +3,28 @@
 import datetime
 import os
 
-from ClassCountFormatter import generate_detailed_statistics_string
+from ClassCountFormatter import generate_detailed_statistics_string, generate_detailed_vehicle_statistics_string
 from ObjectLocationFormatter import generate_objects_location_string
-from get_UAV_status import get_UAV_status
+from get_UAV_status49 import get_UAV_status
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 import time
 import cv2
+import ffmpeg
+import time
 import supervision as sv
 import threading
 import numpy as np
+import subprocess as sp
+import torch
 from alarm import cap_alarm
 from ultralytics.models import YOLO
 from draw import test_corner_boxes
-from vehicle_alarm import vehicle_alarm
-
+from vehicle_alarm import vehicle_alarm, vehicle_alarm_last
 
 # ----------------函数（等待放置于相关工具类）----------------------------------
 # 对于每一帧的处理
-def process_frame(results):
+def process_frame(results, max_id_per_class, count_per_class):
     # 获取当前帧的类别ID和跟踪ID
     class_ids = results.boxes.cls.cpu().numpy().astype(int)
     track_ids = results.boxes.id.cpu().numpy().astype(int)
@@ -35,148 +38,220 @@ def process_frame(results):
             # 对于每个新的track_id，增加该类别的计数
             count_per_class[class_id] += 1
 
+def probe_rtmp_stream(url):
+    try:
+        ffmpeg.probe(url)
+    except ffmpeg.Error as e:
+        pass
 
-# ----------------路径设置----------------------------------
-source_path = "E:/videos_for_test/DJI_20240416155626_0011_V.mp4"  # 视频路径
-# weights_path = "best/best1.pt"  # 权重文件路径
-weights_path = "weights/yolov9c-1280-bathch2-all.pt"
-save_path = "runs"  # 运行结果保存路径
-tracker = "botsort.yaml"  # 所采用的跟踪算法
-save_directory = "runs/screenshot"  # 异常图片保存路径
-vehicle_directory = "runs/vehicle_screenshot"
+def detect(UAV_status):
+    # ----------------路径设置----------------------------------
+    source_path = "E:/videos_for_test/prj49/target2.mp4"  # 视频路径
+    # weights_path = "best/best1.pt"  # 权重文件路径
+    weights_path = "weights/yolov9c_TFS-1280-batch4-400e-prj28.pt"
+    save_path = "runs"  # 运行结果保存路径
+    tracker = "botsort.yaml"  # 所采用的跟踪算法
+    save_directory = "runs/screenshot"  # 异常图片保存路径
+    vehicle_directory = "runs/vehicle_screenshot"
+    rtsp_url = 'rtsp://10.14.197.102:8554/' # RTSP服务器地址
 
-# ----------------参数设置------------------------------------
-# 设置报警帧间隔
-alarm_clock = 30
-# 设置人数变动阈值
-person_increase_or_decrease = 10
-# 帧计数
-frame_count = 0
-# 人数变动计数
-person_change_count = 0
-# 初始人数
-num_person = 0
+    # ----------------参数设置------------------------------------
+    # 设置报警帧间隔
+    alarm_clock = 30
+    # 设置人数变动阈值
+    person_increase_or_decrease = 10
+    # 帧计数
+    frame_count = 0
+    # 人数变动计数
+    person_change_count = 0
+    # 初始人数
+    num_person = 0
+    rtmp_sub_url = str(UAV_status["data"]["rtmpSubUrl"])
 
-# 获取无人机状态
-UAV_status = get_UAV_status()
+    # ----------------初始化------------------------------------
+    # 每个类别当前的最大ID
+    max_id_per_class = {i: -1 for i in range(11)}  # 假设类别ID范围为0到10
+    # 每个类别的计数
+    count_per_class = {i: 0 for i in range(11)}
+    # 用于计时：每隔五秒输出一次各个类的累计数量
+    p_start = time.time()
 
-# ----------------初始化------------------------------------
-# 每个类别当前的最大ID
-max_id_per_class = {i: -1 for i in range(9)}  # 假设类别ID范围为0到8
-# 每个类别的计数
-count_per_class = {i: 0 for i in range(9)}
-# 用于计时：每隔五秒输出一次各个类的累计数量
-p_start = time.time()
+    # 初始化FFmpeg推流
+    # cap = cv2.VideoCapture(rtmp_sub_url)
+    # fps = int(cap.get(cv2.CAP_PROP_FPS))
+    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # command = [
+    #     'ffmpeg',
+    #     '-y',
+    #     '-f', 'rawvideo',
+    #     '-pix_fmt', 'bgr24',
+    #     '-s', "{}x{}".format(width, height),
+    #     '-r', str(fps),
+    #     '-i', '-',
+    #     '-c:v', 'libx264',
+    #     '-pix_fmt', 'yuv420p',
+    #     '-preset', 'ultrafast',
+    #     '-f', 'rtsp',
+    #     rtsp_url
+    # ]
+    # p = sp.Popen(command, stdin=sp.PIPE)
 
-# ------------------推理模块-----------------------------------
-start = time.time()
-model = YOLO(weights_path)
-classes = [i for i in range(9)]
-results = model.track(
-    source=source_path,
-    stream=True,  # 流模式处理，防止因为因为堆积而内存溢出
-    show=True,  # 实时推理演示
-    tracker=tracker,  # 默认tracker为botsort
-    save=True,
-    save_dir=save_path,
-    # vid_stride=2,  # 视频帧数的步长，即隔几帧检测跟踪一次
-    # save_txt=True,  # 把结果以txt形式保存
-    # save_conf=True,  # 保存置信度得分
-    classes=classes,  # 忽略’火焰‘和’烟雾‘类别
-    save_crop=True,  # 保存剪裁的图像
-    conf=0.4,
-    iou=0.5,
-    device=0,
-)
 
-"""
-# result.names输出的class为：
-# {0: 'person', 1: 'helmet', 2: 'life jacket', 3: 'truck', 4: 'excavator', 、
-# 5: 'car crane', 6: 'crawler crane', 7: 'rotary drill rig', 8: 'concrete tanker', 9: 'flame', 10: 'smoke'}
-# 其中 0:person 和 1:helmet 是重点要处理的
-"""
+    # ------------------推理模块-----------------------------------
+    start = time.time()
+    model = YOLO(weights_path)
+    classes = [i for i in range(9)]
+    results = model.track(
+        source=rtmp_sub_url,
+        # source=source_path,
+        stream=True,  # 流模式处理，防止因为因为堆积而内存溢出
+        show=True,  # 实时推理演示
+        tracker=tracker,  # 默认tracker为botsort
+        save=True,
+        save_dir=save_path,
+        # vid_stride=2,  # 视频帧数的步长，即隔几帧检测跟踪一次
+        # save_txt=True,  # 把结果以txt形式保存
+        # save_conf=True,  # 保存置信度得分
+        # save_crop=True,  # 保存剪裁的图像
+        classes=classes,  # 忽略’火焰‘和’烟雾‘类别
+        conf=0.4,
+        iou=0.5,
+        device=0,
+    )
 
-for r in results:
-    frame_count += 1
+    """
+    # result.names输出的class为：
+    # {0: 'person', 1: 'helmet', 2: 'life jacket', 3: 'truck', 4: 'excavator', 、
+    # 5: 'car crane', 6: 'crawler crane', 7: 'rotary drill rig', 8: 'concrete tanker', 9: 'flame', 10: 'smoke'}
+    # 其中 0:person 和 1:helmet 是重点要处理的
+    """
 
-    boxes = r.boxes  # Boxes object for bbox outputs
-    masks = r.masks  # Masks object for segment masks outputs
-    probs = r.probs  # Class probabilities for classification outputs
+    for r in results:
+        frame_count += 1
 
-    # 得到某一帧的检测结果(转化为PIL图片)
-    im_array = r.plot(conf=False, line_width=1, font_size=1.5)  # plot a BGR numpy array of predictions
-    # im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-    # im.show()  # show image
-    # im.save(det_img_path)  # save image
+        boxes = r.boxes  # Boxes object for bbox outputs
+        masks = r.masks  # Masks object for segment masks outputs
+        probs = r.probs  # Class probabilities for classification outputs
 
-    if r.boxes.id is not None:  # boxes.id:盒子的轨道id(如果可以被跟踪算法检测的话就为True)。
-        # ----------------------对各个类别进行累计计数------------------------
-        # 得放进来否则在r.boxes.id为None的时候会报错
-        process_frame(r)
-        # 打印累计计数
-        # print(count_per_class)
-        if time.time() - p_start >= 6:
-            # ---------------------先保存numpy数组格式的图像--------------------
-            # 形成文件名
-            filename = str(int(time.time())) + ".png"  # 使用当前时间的时间戳（无小数点）
-            _file_path = os.path.join(vehicle_directory, filename)
+        # 得到某一帧的检测结果(转化为PIL图片)
+        im_array = r.plot(conf=False, line_width=1, font_size=1.5)  # plot a BGR numpy array of predictions
+        # im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        # im.show()  # show image
+        # im.save(det_img_path)  # save image
 
-            # 保存图像
-            cv2.imwrite(_file_path, im_array)  # OpenCV的方法来保存numpy数组格式的图像
-            # ---------------------------------------------------------------
+        # 将处理后的帧推送到RTSP服务器
+        # p.stdin.write(im_array.tobytes())
+        # print("111")
 
-            # vehicle_alarm(_file_path, count_per_class)
-            p_start = time.time()
-            # print("6秒时间到")
-        # ----------------------------------------------------------------
+        if r.boxes.id is not None:  # boxes.id:盒子的轨道id(如果可以被跟踪算法检测的话就为True)。
+            # ----------------------对各个类别进行累计计数------------------------
+            # 得放进来否则在r.boxes.id为None的时候会报错
+            process_frame(r, max_id_per_class, count_per_class)
+            # 打印累计计数
+            # print(count_per_class)
 
-        class_ids = r.boxes.cls.cpu().numpy().astype(int)
-        class_ids_string = generate_detailed_statistics_string(class_ids)
-        objects_location_string = generate_objects_location_string(boxes.xyxy, boxes.cls)
+            # ----------------------------------------------------------------
 
-        # ----------------------------------------------------------------
+            class_ids = r.boxes.cls.cpu().numpy().astype(int)
+            class_ids_string = generate_detailed_statistics_string(class_ids)
+            class_ids_vehicle_string = generate_detailed_vehicle_statistics_string(class_ids)
+            objects_location_string = generate_objects_location_string(boxes.xyxy, boxes.cls)
 
-        # 首先保存上一帧的人数
-        last_frame_persons = num_person
-        # 接下来对目前帧的person和helmet个数进行统计
-        num_person = np.sum(class_ids == 0)
-        num_helmet = np.sum(class_ids == 1)
-        # 统计变动人数
-        person_change_count += abs(last_frame_persons - num_person)
-        # 报警条件判断
-        # 时间/检测帧数足够了,若检测到未戴安全帽立即警报
-        if frame_count >= alarm_clock:
-            # ----------------------处理选框------------------------
-            out_img = test_corner_boxes(im_array, boxes.xywh, boxes.xyxy, boxes.cls, boxes.id, l=5,
-                                        is_transparent=True,
-                                        draw_type=True, draw_corner=True)
-            # 形成文件名
-            filename = str(int(time.time())) + ".png"  # 使用当前时间的时间戳（无小数点）
-            file_path = os.path.join(save_directory, filename)
-            # 保存图像
-            cv2.imwrite(file_path, out_img)  # OpenCV的方法来保存numpy数组格式的图像
-            # -----------------------------------------------------
-            person_change_count = 0
-            print(objects_location_string)
-            # cap_alarm(UAV_status, file_path, num_person, num_helmet, class_ids_string, objects_location_string)
-            print("此帧异常检测图片已保存至", file_path, ",并同步上传至数据库。")
-            frame_count = 0
+            if time.time() - p_start >= 30:
+                # ---------------------先保存numpy数组格式的图像--------------------
+                # 形成文件名
+                filename = str(int(time.time())) + ".png"  # 使用当前时间的时间戳（无小数点）
+                _file_path = os.path.join(vehicle_directory, filename)
 
-# ---------------------推理结束调vehicle_alarm接口---------------------
-# 先保存numpy数组格式的图像
-# 形成文件名
-filename = str(int(time.time())) + ".png"  # 使用当前时间的时间戳（无小数点）
-_file_path = os.path.join(vehicle_directory, filename)
+                # 保存图像
+                cv2.imwrite(_file_path, im_array)  # OpenCV的方法来保存numpy数组格式的图像
+                # ---------------------------------------------------------------
 
-# 保存图像
-cv2.imwrite(_file_path, im_array)  # OpenCV的方法来保存numpy数组格式的图像
-# ---------------------------------------------------------------
-print(objects_location_string)
-# vehicle_alarm(_file_path, count_per_class, UAV_status, class_ids_string, objects_location_string)
-# ----------------------------------------------------------------
+                # vehicle_alarm(_file_path, count_per_class, UAV_status, class_ids_vehicle_string, objects_location_string)
+                p_start = time.time()
+                # print("6秒时间到")
 
-end = time.time()
-print("推理所用时间:", end - start)
+            # ----------------------------------------------------------------
+
+            # 首先保存上一帧的人数
+            last_frame_persons = num_person
+            # 接下来对目前帧的person和helmet个数进行统计
+            num_person = np.sum(class_ids == 0)
+            num_helmet = np.sum(class_ids == 1)
+            # 统计变动人数
+            person_change_count += abs(last_frame_persons - num_person)
+            # 报警条件判断
+            # 时间/检测帧数足够了,若检测到未戴安全帽立即警报
+            if frame_count >= alarm_clock:
+                # ----------------------处理选框------------------------
+
+                # 得到某一帧的检测结果(转化为PIL图片)
+                im_alarm_array = r.plot(conf=False, line_width=1, font_size=1.5,
+                                        vehicle=False)  # plot a BGR numpy array of
+                out_img = test_corner_boxes(im_alarm_array, boxes.xywh, boxes.xyxy, boxes.cls, boxes.id, l=5,
+                                            is_transparent=True,
+                                            draw_type=True, draw_corner=True)
+                # 形成文件名
+                filename = str(int(time.time())) + ".png"  # 使用当前时间的时间戳（无小数点）
+                file_path = os.path.join(save_directory, filename)
+                # 保存图像
+                cv2.imwrite(file_path, out_img)  # OpenCV的方法来保存numpy数组格式的图像
+                # -----------------------------------------------------
+                person_change_count = 0
+                #print(objects_location_string)
+                # cap_alarm(UAV_status, file_path, num_person, num_helmet, class_ids_string, objects_location_string)
+                print("此帧异常检测图片已保存至", file_path, ",并同步上传至数据库。")
+                frame_count = 0
+
+
+
+    # ---------------------推理结束调vehicle_alarm接口---------------------
+    # 先保存numpy数组格式的图像
+    # 形成文件名
+    filename = str(int(time.time())) + ".png"  # 使用当前时间的时间戳（无小数点）
+    _file_path = os.path.join(vehicle_directory, filename)
+
+    # 保存图像
+    cv2.imwrite(_file_path, im_array)  # OpenCV的方法来保存numpy数组格式的图像
+    # ---------------------------------------------------------------
+    # vehicle_alarm_last(_file_path, count_per_class, UAV_status, None, None)
+    # ----------------------------------------------------------------
+
+    end = time.time()
+    print("推理所用时间:", end - start)
+
+
+def check_rtmp_stream():
+    while True:
+        uav_status = get_UAV_status()
+        uav_data = uav_status["data"]
+        if uav_data:
+            url = uav_status["data"]["rtmpSubUrl"]
+            if url:
+                print("RTMP stream: " + str(url) + " is available.")
+                detect(uav_status)
+                time.sleep(60)
+            else:
+                print("RTMP stream is not available.")
+                time.sleep(60)
+        else:
+            print("RTMP stream is not available.")
+            time.sleep(60)
+
+# check_rtmp_stream()
+
+if __name__ == "__main__":
+    UAV_status = {
+        "data": {
+            "planId": "test",
+            "time": "2024-01-01T00:00:00",
+            "uavRecordId": "test",
+            "projectId": "test",
+            "rtmpSubUrl": "E:/videos_for_test/prj49/target2.mp4"
+        }
+    }
+    detect(UAV_status)
 # -----------------------------------------------------------------------------------------------------------------------------
 # 同一帧检测到多个对象且可被跟踪的情况
 # 其中要处理的是
